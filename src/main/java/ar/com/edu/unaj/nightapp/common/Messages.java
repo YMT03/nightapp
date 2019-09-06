8324f0791fe3d54e036253822f0408a8505024c6
YMT03 <nachito.eze.ramos@gmail.com>
2019-09-06 15:06:58 -0300

    msg update

--- src/main/java/ar/com/edu/unaj/nightapp/common/Messages.java
+++ src/main/java/ar/com/edu/unaj/nightapp/common/Messages.java
@@ -3,7 +3,7 @@ package ar.com.edu.unaj.nightapp.common;
 public class Messages {
 
     //Exceptions
-    public static final String EstablecimientoNotFoundException ="El Establecimiento solicitado no existe";
+    public static final String ESTABLECIMIENTO_NOT_FOUND_EXCEPTION ="El Establecimiento solicitado no existe";
 
     //Validations
     //TODO
--- src/main/java/ar/com/edu/unaj/nightapp/exception/EstablecimientoNotFoundException.java
+++ src/main/java/ar/com/edu/unaj/nightapp/exception/EstablecimientoNotFoundException.java
@@ -4,6 +4,6 @@ import ar.com.edu.unaj.nightapp.common.Messages;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason= Messages.EstablecimientoNotFoundException)
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason= Messages.ESTABLECIMIENTO_NOT_FOUND_EXCEPTION)
 public class EstablecimientoNotFoundException extends RuntimeException {
 }
