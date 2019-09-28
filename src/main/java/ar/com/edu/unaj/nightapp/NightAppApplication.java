5d92770ba96f01d55ea72418d04469502cf52462
YMT03 <nachito.eze.ramos@gmail.com>
2019-09-27 22:27:15 -0300

    Se permiten conexiones desde protocolos no http. Por cuestiones de integracion

--- src/main/java/ar/com/edu/unaj/nightapp/NightAppApplication.java
+++ src/main/java/ar/com/edu/unaj/nightapp/NightAppApplication.java
@@ -3,12 +3,26 @@ package ar.com.edu.unaj.nightapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class NightAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NightAppApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
